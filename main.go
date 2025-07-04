13453768c1d4b4b8db396ce7a421c7cff971eebe
krishnaShuk <shuklakrishna.kris@gmail.com>
2025-07-04 22:52:52 +0530

    Functional api

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/krishnashuk/Go-MongoDB/controllers"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	fmt.Printf("Connected to MongoDB at %s\n", "mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	s.SetMode(mgo.Monotonic, true)
+	return s
+}
